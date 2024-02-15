9180f49928f4b9dfa07b8cf8c6bc6ab9a2201bea
Med95B <bouizloufa@gmail.com>
2024-02-15 18:20:16 +0100

    py mongo

--- mongoReq.py
+++ mongoReq.py
@@ -1,8 +1,12 @@
 from pymongo import MongoClient
 client=MongoClient()
 print(client)
+print('===============================================')
+
 db=client['DBSportifs']
 print(db)
+print('===============================================')
+
 resultat=db.Sportif.find()
 for r in resultat:
     print(r)
@@ -12,3 +16,7 @@ selection['genre']='homme'
 resultat=db.Sportif.find(selection)
 for r in resultat:
     print (r)
+print('===============================================')
+r=db.Sportif.find().sort('nom',-1)
+for i in r:
+    print(i)
